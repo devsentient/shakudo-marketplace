818f421f9cc85049501bae05b827feca3359b18a
patrick-shakudo <patrick@shakudo.io>
2024-03-08 16:57:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# shakudo-marketplace
+Shakudo deployment configuration, user guide, and resources for cloud marketplace listings
---
+++ gcp/scripts/pre_gcp_mp_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source ./scripts/ordered_static_resources.sh
+
+function apply_k8_resources() {
+    # Create namespaces
+    kubectl apply -f $shakudo_helm_chart_path/static/namespaces/
+
+    # Apply CRDs
+    kubectl apply -f $shakudo_helm_chart_path/static/crds/
+    kubectl apply -f $shakudo_helm_chart_path/static/prometheus/
+    kubectl apply -f $shakudo_helm_chart_path/static/cert-manager/cert-manager.yaml
+
+    create_secrets
+    create_configmaps   
+    create_rbac
+
+    create_gitserver
+    create_cluster_issuer
+    create_istio_service_mesh
+
+    kubectl apply -f $shakudo_helm_chart_path/static/priorityClasses
+}
+
+# Disallow expansion of unset variables
+set -o nounset
+
+# Exit upon error
+set -e
+
+init_paths
+apply_k8_resources
